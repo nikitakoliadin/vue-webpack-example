e9f034b12100ae91df51397f41093e824a7c588f
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-25 18:49:59 +0300

    Refactoring

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -37,7 +37,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'HelloWorld',
@@ -52,14 +52,17 @@ export default defineComponent({
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
